184c0f8a9dadba5788f3e6143b6ef52d72b57dde
아이스본 <tlrxkrajrwk@gmail.com>
2023-06-10 14:46:56 +0900

    Create IchenTourist.java

---
+++ src/Main/IchenTourist.java
@@ -0,0 +1,39 @@
+package Main;
+
+public class IchenTourist {
+	
+	private String name;
+	private String address;
+	private String photo;
+	private String tag;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	
+	
+}
